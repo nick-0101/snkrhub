5962da959a7fd4e98754f13b283e3d3f870cecf1
phoenixbeats01 <nicolapirruccio@icloud.com>
2022-04-13 19:39:35 -0400

    got typescript working with docker

--- backend/inventory-microservice/src/server.ts
+++ backend/inventory-microservice/src/server.ts
@@ -5,12 +5,14 @@ const app = express()
 const router = Router()
 
 router.get('/', async(req, res)=>{
-    res.status(200).json({message : 'Hello world'})
+    res.status(200).json({
+        message : 'hi'
+    })
 })
 
 app.use('/api', router)
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3232
 
 app.listen(port, ()=>{
     console.log(`server listening on http://localhost:${port}`)
