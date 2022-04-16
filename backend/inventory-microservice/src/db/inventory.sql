aed5b7453e97d942cd6656d096efed8b46f7aa5b
phoenixbeats01 <nicolapirruccio@icloud.com>
2022-04-16 09:27:39 -0400

    added postgres database

---
+++ backend/inventory-microservice/src/db/inventory.sql
@@ -0,0 +1,15 @@
+CREATE TABLE inventory (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id VARCHAR(128) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    styleId VARCHAR(25),
+    brand VARCHAR(25),
+    colour VARCHAR(25),
+    condition VARCHAR(25),
+    shoeSize DECIMAL,
+    purchasePrice DECIMAL,
+    tax DECIMAL,
+    shipping DECIMAL,
+    purchaseDate DATE NOT NULL,
+    orderNumber INTEGER
+)
