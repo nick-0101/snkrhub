82879888326b82c8a4dab1544c60c1a27795efc4
phoenixbeats01 <nicolapirruccio@icloud.com>
2022-04-17 20:39:57 -0400

    mutation tests working

--- backend/inventory-microservice/src/tests/ApolloServer.test.ts
+++ backend/inventory-microservice/src/tests/ApolloServer.test.ts
@@ -9,40 +9,39 @@ beforeAll(async () => {
   await db.sync({ force: true })
 })
 
-it('returns hello with the provided name', async () => {
-  const testServer = new ApolloServer({
-    typeDefs,
-    resolvers
-  });
+describe('inventory microservice e2e', () => {
+  it('creates an inventory item with addInventoryItem mutation', async () => {
+    const testServer = new ApolloServer({
+      typeDefs,
+      resolvers
+    });
 
-  const result = await testServer.executeOperation({
-    query: `
-    mutation AddInventoryItem($inventoryItem: InventoryItemInput!) {
-      addInventoryItem(inventoryItem: $inventoryItem) {
-        id
+    const result = await testServer.executeOperation({
+      query: `
+      mutation AddInventoryItem($inventoryItem: InventoryItemInput!) {
+        addInventoryItem(inventoryItem: $inventoryItem) {
+          id
+        }
       }
-    }
-    `,
-    variables: {
-      inventoryItem: {
-        user_id: "askurhwuyg",
-        name: "Nike air force",
-        styleid: "DHAG",
-        brand: "Nike",
-        colour: "Black/White",
-        condition: "Used",
-        shoesize: 11,
-        purchaseprice: 110,
-        tax: 10,
-        shipping: 0.00,
-        purchasedate: "2022-11-02",
-        ordernumber: "sadfsdfsdf"
-      }
-    },
+      `,
+      variables: {
+        inventoryItem: {
+          user_id: "askurhwuyg",
+          name: "Nike air force",
+          styleid: "DHAG",
+          brand: "Nike",
+          colour: "Black/White",
+          condition: "Used",
+          shoesize: 11,
+          purchaseprice: 110,
+          tax: 10,
+          shipping: 0.00,
+          purchasedate: "2022-11-02",
+          ordernumber: "sadfsdfsdf"
+        }
+      },
+    });
+
+    expect(result.data?.addInventoryItem).toMatchObject({ "id": "1"});
   });
-
-  console.log(result)
-
-  // expect(result.errors).toBeUndefined();
-  expect(result.data?.addInventoryItem).toBe({ "id": "1"});
 });
