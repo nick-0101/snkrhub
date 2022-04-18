910a903e785e6986888b1b25dded3dbc52e2d221
phoenixbeats01 <nicolapirruccio@icloud.com>
2022-04-18 19:52:04 -0400

    wrote tests for delete mutation

--- backend/inventory-microservice/src/tests/ApolloServer.test.ts
+++ backend/inventory-microservice/src/tests/ApolloServer.test.ts
@@ -6,49 +6,6 @@ const { typeDefs } = require('../schema/typeDefs')
 const db = require('../clients/postgres');
 const Inventory = require('../models/inventoryModel');
 
-describe('tests creating inventory items', () => {
-  beforeAll(async () => {
-    // Regenreate database
-    await db.sync({ force: true })
-  })
-
-  it('creates an inventory item with addInventoryItem mutation', async () => {
-    const testServer = new ApolloServer({
-      typeDefs,
-      resolvers,
-      context: () => ({ userId: 'testUserId' }),
-    });
-
-    const result = await testServer.executeOperation({
-      query: `
-        mutation AddInventoryItem($inventoryItem: InventoryItemInput!) {
-          addInventoryItem(inventoryItem: $inventoryItem) {
-            id
-          }
-        }
-      `,
-      variables: {
-        inventoryItem: {
-          user_id: "testUserId",
-          name: "Nike air force",
-          styleid: "DHAG",
-          brand: "Nike",
-          colour: "Black/White",
-          condition: "Used",
-          shoesize: 11,
-          purchaseprice: 110,
-          tax: 10,
-          shipping: 0.00,
-          purchasedate: "2022-11-02",
-          ordernumber: "sadfsdfsdf"
-        }
-      },
-    });
-
-    expect(result.data?.addInventoryItem).toMatchObject({ "id": 1 });
-  });
-});
-
 describe('tests reading inventory items', () => {  
   beforeAll(async () => {
     // Regenreate database
@@ -100,6 +57,74 @@ describe('tests reading inventory items', () => {
   });
 })
 
+describe('tests creating inventory items', () => {
+  beforeAll(async () => {
+    // Regenreate database
+    await db.sync({ force: true })
+  })
+
+  it('creates an inventory item with addInventoryItem mutation', async () => {
+    const testServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: () => ({ userId: 'testUserId' }),
+    });
+
+    const result = await testServer.executeOperation({
+      query: `
+        mutation AddInventoryItem($inventoryItem: InventoryItemInput!) {
+          addInventoryItem(inventoryItem: $inventoryItem) {
+            id
+          }
+        }
+      `,
+      variables: {
+        inventoryItem: {
+          user_id: "testUserId",
+          name: "Nike air force",
+          styleid: "DHAG",
+          brand: "Nike",
+          colour: "Black/White",
+          condition: "Used",
+          shoesize: 11,
+          purchaseprice: 110,
+          tax: 10,
+          shipping: 0.00,
+          purchasedate: "2022-11-02",
+          ordernumber: "sadfsdfsdf"
+        }
+      },
+    });
+
+    expect(result.data?.addInventoryItem).toMatchObject({ "id": 1 });
+  });
+});
+
+// Write tests for delete, then start on client
+describe('tests deleting inventory items', () => {
+  it('deletes an inventory item with deleteInventoryItem mutation', async () => {
+    const testServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: () => ({ userId: 'testUserId' }),
+    });
+
+    const result = await testServer.executeOperation({
+      query: `
+        mutation DeleteInventoryItem($itemId: Int!) {
+          deleteInventoryItem(itemId: $itemId) {
+            id  
+          }
+        }
+      `,
+      variables: {
+        itemId: 1
+      },
+    });
+
+    expect(result.data?.deleteInventoryItem).toMatchObject({ "id": 1 });
+  });
+});
 
 // Closing the DB connection allows Jest to exit successfully.
 afterAll((done) => {
