981d8b32320ec31da566bd576ac64b3002bf081b
phoenixbeats01 <nicolapirruccio@icloud.com>
2022-04-24 19:20:55 -0400

    express server running on analytics microservice

---
+++ backend/analytics-microservice/src/clients/postgres.ts
@@ -0,0 +1,16 @@
+export {}
+
+const { Sequelize } = require('sequelize');
+const {
+  POSTGRES_DATABASE,
+  POSTGRES_USERNAME,
+  POSTGRES_PASSWORD,
+  POSTGRES_IP,
+  POSTGRES_PORT,
+} = require('../config');
+
+// connect to postgres database
+const postgresURL = POSTGRES_PASSWORD ? `postgres://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_IP}:${POSTGRES_PORT}/${POSTGRES_DATABASE}` : process.env.DATABASE_URL;
+const db = new Sequelize(postgresURL, { logging: false });
+
+module.exports = db;
