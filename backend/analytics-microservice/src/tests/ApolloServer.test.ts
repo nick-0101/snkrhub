4365bdb6085832c753cf445143a9f9f652d991bc
phoenixbeats01 <nicolapirruccio@icloud.com>
2022-05-02 21:00:27 -0400

    added tests for all apollo operations on analytics microservice

--- backend/analytics-microservice/src/tests/ApolloServer.test.ts
+++ backend/analytics-microservice/src/tests/ApolloServer.test.ts
@@ -9,6 +9,7 @@ const db = require('../clients/postgres');
 const InventoryAnalytics = require('../models/InventoryAnalyticsModel');
 const InventoryValue = require('../models/InventoryValueModel');
 
+// Queries
 describe('tests querying inventory analytics', () => {  
     beforeAll(async () => {
         // Regenreate database
@@ -53,7 +54,35 @@ describe('tests querying inventory analytics', () => {
         ]);
     })
 
-        it('queries a range of analytics from inventory_value table', async () => {
+    it('queries basic stats from inventory_analytics table', async () => {
+        const testServer = new ApolloServer({
+            typeDefs,
+            resolvers,
+            context: () => ({ userId: 'testUserId' }),
+        });
+
+        const result = await testServer.executeOperation({
+            query: `
+                query FetchInventoryAnalytics {
+                    fetchInventoryAnalytics {
+                        inventorycount
+                        inventorysold
+                        inventoryvalue
+                        itemspend
+                    }
+                }
+            `
+        });
+
+        expect(result.data?.fetchInventoryAnalytics).toMatchObject({ 
+            inventorycount: 5,
+            itemspend: 2500,
+            inventorysold: 1,
+            inventoryvalue: 2000 
+        });
+    });
+
+    it('queries a range of analytics from inventory_value table', async () => {
         const testServer = new ApolloServer({
             typeDefs,
             resolvers,
@@ -93,8 +122,32 @@ describe('tests querying inventory analytics', () => {
             }
         ]);
     });
+})
 
-    it('queries basic stats from inventory_analytics table', async () => {
+// Mutations
+describe('tests inventory analytics mutations', () => {
+    beforeAll(async () => {
+        // Regenreate database
+        await db.sync({ force: true });
+
+        // Insert into inventory_analytics table
+        await InventoryAnalytics.create({ 
+            user_id: 'testUserId',
+            inventorycount: 1,
+            itemspend: 200,
+            inventorysold: 0,
+            inventoryvalue: 200 
+        })
+
+        // Insert into inventory_value table
+        await InventoryValue.create({ 
+            user_id: 'testUserId',
+            inventoryvalue: 0,
+            createdAt: dayjs().locale('en').format("YYYY-MM-DD")
+        });
+    })
+
+    it('updates analytics stats on item add', async () => {
         const testServer = new ApolloServer({
             typeDefs,
             resolvers,
@@ -103,25 +156,65 @@ describe('tests querying inventory analytics', () => {
 
         const result = await testServer.executeOperation({
             query: `
-                query FetchInventoryAnalytics {
-                    fetchInventoryAnalytics {
-                        inventorycount
-                        inventorysold
-                        inventoryvalue
-                        itemspend
-                    }
+                mutation UpdateAnalyticsForItemAdd($inventoryItem: InventoryAnalyticsItemAddInput!) {
+                    updateAnalyticsForItemAdd(inventoryItem: $inventoryItem)
                 }
-            `
-        });
+            `,
+            variables: {
+                inventoryItem: {
+                    purchaseprice: 200
+                }
+            },
+        })
 
-        expect(result.data?.fetchInventoryAnalytics).toMatchObject({ 
-            inventorycount: 5,
-            itemspend: 2500,
-            inventorysold: 1,
-            inventoryvalue: 2000 
-        });
+        expect(result.data?.updateAnalyticsForItemAdd).toBe('updated stats')
     });
 
+    it('updates analytics stats on item delete', async () => {
+        const testServer = new ApolloServer({
+            typeDefs,
+            resolvers,
+            context: () => ({ userId: 'testUserId' }),
+        });
+
+        const result = await testServer.executeOperation({
+            query: `
+                mutation UpdateAnalyticsForItemDelete($inventoryItem: InventoryAnalyticsItemDelete!) {
+                    updateAnalyticsForItemDelete(inventoryItem: $inventoryItem)
+                }
+            `,
+            variables: {
+                inventoryItem: {
+                    purchaseprice: 200
+                }
+            },
+        })
+
+        expect(result.data?.updateAnalyticsForItemDelete).toBe('updated stats')
+    });
+
+    it('updates analytics stats on item marked sold', async () => {
+        const testServer = new ApolloServer({
+            typeDefs,
+            resolvers,
+            context: () => ({ userId: 'testUserId' }),
+        });
+
+        const result = await testServer.executeOperation({
+            query: `
+                mutation UpdateAnalyticsForItemSold($inventoryItem: InventoryAnalyticsItemSold!) {
+                    updateAnalyticsForItemSold(inventoryItem: $inventoryItem)
+                }
+            `,
+            variables: {
+                inventoryItem: {
+                    purchaseprice: 200
+                }
+            },
+        })
+
+        expect(result.data?.updateAnalyticsForItemSold).toBe('marked sold')
+    });
 })
 
 // Closing the DB connection allows Jest to exit successfully.
